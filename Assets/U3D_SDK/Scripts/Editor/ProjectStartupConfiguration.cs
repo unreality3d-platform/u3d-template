082e73c2b797115d3ac6153c56dc365390b9273b
Laurie Annis <laurieannis@yahoo.com>
2025-06-20 16:29:16 -0700

    Added StartupConfiguration file to ensure WebGL target and _MyScene are opened on first open

---
+++ Assets/U3D_SDK/Scripts/Editor/ProjectStartupConfiguration.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class ProjectStartupConfiguration
+{
+    private const string STARTUP_SCENE_PATH = "Assets/Scenes/_My Scene.unity";
+    private const string PREF_KEY = "HasConfiguredStartup";
+
+    static ProjectStartupConfiguration()
+    {
+        EditorApplication.delayCall += ConfigureProjectStartup;
+    }
+
+    private static void ConfigureProjectStartup()
+    {
+        if (!EditorPrefs.GetBool(PREF_KEY, false))
+        {
+            // Set build target to WebGL
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL);
+
+            // Open startup scene
+            if (System.IO.File.Exists(STARTUP_SCENE_PATH))
+            {
+                EditorSceneManager.OpenScene(STARTUP_SCENE_PATH);
+            }
+
+            EditorPrefs.SetBool(PREF_KEY, true);
+        }
+    }
+}
